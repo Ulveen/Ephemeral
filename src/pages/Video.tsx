b1c2feb8e43118c1bcb2301b53897b250c184247
Ulveen <131880915+Ulveen@users.noreply.github.com>
2024-06-20 01:42:47 +0700

    Update Video.tsx

--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -4,7 +4,10 @@ export default function Video() {
     return (
         <div className="page" id="video">
             <p className="video-title">Video</p>
-            <iframe className='video-vid' src="" />
+            <iframe className='video-vid'
+                src="https://www.youtube.com/embed/UPjZSPh04QM"
+                allowFullScreen
+            />
         </div>
     )
 }
